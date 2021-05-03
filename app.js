706eba7ada63c1bce0989de862af180c2617e1c4
Oleksandr Zoria <oleksandr.zoria@p2h.com>
2021-05-03 18:56:34 +0300

    updated cors url

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 // Enable CORS
 app.use(cors({
   credentials: true,
-  origin: 'http://localhost:8080'
+  origin: 'https://free-psy.herokuapp.com'
 }));
 
 // app.use(cors())
