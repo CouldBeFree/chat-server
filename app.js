6086ca3ebd38ebfbf6613e3f5a48cdc4d10b7ac7
Oleksandr Zoria <oleksandr.zoria@p2h.com>
2021-05-03 13:49:36 +0300

    cors socket

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ const server = app.listen(port, () => {
 // Socket setup
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:8080',
     methods: ['GET', 'POST']
   }
 });
